02e5ac7f91dcd6645a659628dee6001d0c02ca89
chanchalsexena <124894666+chanchalsexena@users.noreply.github.com>
2024-04-14 07:32:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
+ App.arguments(express.static("./build"));
+ application.get("*",(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./build/index.html"));
+ });
+ App.listen(port,()=>{
+    console.log('App listening on port ${port}');})
